fc8fe7bded714e96f6ea23eb57d6e52e29c09568
Nikolina Todorova <nikolina.todorova@initos.com>
2018-09-07 13:41:20 +0200

    Add copyright.

--- hr_holidays_public_generator_de/tests/common.py
+++ hr_holidays_public_generator_de/tests/common.py
@@ -1,3 +1,5 @@
+#  Copyright 2018 elego Software Solutions GmbH - Yu Weng
+#  Copyright 2018 initOS GmbH - Nikolina Todorova
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests import common
