ad09c52ec19fcf0750781d7380bbd617b4606a2c
Nikolina Todorova <nikolina.todorova@initos.com>
2018-09-07 10:10:25 +0200

    Add tests to hr_holidays_public_generator_de

---
+++ hr_holidays_public_generator_de/tests/test_hr_holidays_public_generator.py
@@ -0,0 +1,53 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.hr_holidays_public_germany.tests.common import \
+    TestHrHolidaysPublicGenerator
+
+
+class TestHrHolidaysPublicGenerator(TestHrHolidaysPublicGenerator):
+
+    def test_action_generate_de_holidays(self):
+        self.hr_holidays_public_generator.action_generate_de_holidays()
+
+        hr_holiday_public = \
+            self.HrHolidaysPublic.search([('year', '=', self.TestYear)])
+        if not hr_holiday_public:
+            hr_holiday_public = None
+
+        self.assertIsNotNone(hr_holiday_public)
+
+        if hr_holiday_public:
+            line_ids = hr_holiday_public.line_ids
+            if not line_ids:
+                line_ids = None
+            self.assertIsNotNone(line_ids)
+
+    def test_action_copy_de_holidays(self):
+        self.hr_holidays_public_generator.action_generate_de_holidays()
+        template_id = \
+            self.HrHolidaysPublic.search([('year', '=', self.TestYear)])[0].id
+
+        # Test Create Public Holidays for 2019 from 2019
+        TestYear = "2019"
+        wizard_data = {
+            "year": TestYear,
+            "country_id": self.CountryId,
+            "template_id": template_id
+        }
+
+        self.HrHolidaysPublicGenerator.create(wizard_data)
+
+        self.hr_holidays_public_generator_copy.action_copy_de_holidays()
+
+        hr_holiday_public = \
+            self.HrHolidaysPublic.search([('year', '=', TestYear)])
+        if not hr_holiday_public:
+            hr_holiday_public = None
+
+        self.assertIsNotNone(hr_holiday_public)
+
+        if hr_holiday_public:
+            line_ids = hr_holiday_public.line_ids
+            if not line_ids:
+                line_ids = None
+            self.assertIsNotNone(line_ids)
